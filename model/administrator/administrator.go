5cdd3cd72ebfc80c4187dc8a2ef81a66753ad420
Wrath-y <805655528@qq.com>
2023-01-10 11:51:09 +0800

    feat: 优化代码

--- model/administrator/administrator.go
+++ model/administrator/administrator.go
@@ -4,8 +4,14 @@ import (
 	"go-blog/model"
 )
 
-func GetUserByName(account string) (*Administrators, error) {
- 	a := &Administrators{}
+type Administrators struct {
+	model.Base
+	Account  string `json:"account"`
+	Password string `json:"password"`
+}
 
- 	return a, model.DB.Self.Where("account = ?", account).First(&a).Error
+func GetUserByName(account string) (*Administrators, error) {
+	a := &Administrators{}
+
+	return a, model.DB.Self.Where("account = ?", account).First(&a).Error
 }
--- model/administrator/struct.go
+++
@@ -1,9 +0,0 @@
-package administrator
-
-import "go-blog/model"
-
-type Administrators struct {
-	model.Base
-	Account 		string  `json:"account"`
-	Password 		string  `json:"password"`
-}
